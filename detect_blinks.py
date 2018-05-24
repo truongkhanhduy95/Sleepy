a3fb519607e6a29785dafab279ee30da6bceb46c
Duy Truong <truongkhanhduy9@gmail.com>
2018-05-24 22:58:42 +0700

    Fix detect grayscale

--- detect_blinks.py
+++ detect_blinks.py
@@ -15,22 +15,23 @@ import dlib
 import cv2
 
 def eye_aspect_ratio(eye):
-	# compute the euclidean distances between the two sets of
-	# vertical eye landmarks (x, y)-coordinates
 	A = dist.euclidean(eye[1], eye[5])
 	B = dist.euclidean(eye[2], eye[4])
-
-	# compute the euclidean distance between the horizontal
-	# eye landmark (x, y)-coordinates
 	C = dist.euclidean(eye[0], eye[3])
 
-	# compute the eye aspect ratio
 	ear = (A + B) / (2.0 * C)
-
-	# return the eye aspect ratio
 	return ear
+
  
-# construct the argument parse and parse the arguments
+def auto_canny(image, sigma=0.63):
+	v = np.median(image)
+ 
+	lower = int(max(0, (1.0 - sigma) * v))
+	upper = int(min(255, (1.0 + sigma) * v))
+	edged = cv2.Canny(image, lower, upper)
+ 
+	return edged
+ 
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--shape-predictor", required=True,
 	help="path to facial landmark predictor")
@@ -38,40 +39,27 @@ ap.add_argument("-v", "--video", type=str, default="",
 	help="path to input video file")
 args = vars(ap.parse_args())
  
-# define two constants, one for the eye aspect ratio to indicate
-# blink and then a second constant for the number of consecutive
-# frames the eye must be below the threshold
-EYE_AR_THRESH = 0.3
-EYE_AR_CONSEC_FRAMES = 3
-
-# initialize the frame counters and the total number of blinks
+EYE_AR_THRESH = 0.25
+EYE_AR_CONSEC_FRAMES = 48
 COUNTER = 0
 TOTAL = 0
 
-# initialize dlib's face detector (HOG-based) and then create
-# the facial landmark predictor
 print("[INFO] loading facial landmark predictor...")
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(args["shape_predictor"])
 
-# grab the indexes of the facial landmarks for the left and
-# right eye, respectively
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
-# start the video stream thread
 print("[INFO] starting video stream thread...")
 vs = FileVideoStream(args["video"]).start()
 fileStream = True
-# vs = VideoStream(src=0).start()
-# vs = VideoStream(usePiCamera=True).start()
-# fileStream = False
+vs = VideoStream(src=0).start()
+fileStream = False
 time.sleep(1.0)
 
 # loop over frames from the video stream
 while True:
-	# if this is a file video stream, then we need to check if
-	# there any more frames left in the buffer to process
 	if fileStream and not vs.more():
 		break
 
@@ -81,6 +69,7 @@ while True:
 	frame = vs.read()
 	frame = imutils.resize(frame, width=450)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	backtorgb = cv2.cvtColor(gray,cv2.COLOR_GRAY2RGB)
 
 	# detect faces in the grayscale frame
 	rects = detector(gray, 0)
@@ -107,34 +96,39 @@ while True:
 		# visualize each of the eyes
 		leftEyeHull = cv2.convexHull(leftEye)
 		rightEyeHull = cv2.convexHull(rightEye)
-		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+		cv2.drawContours(backtorgb, [leftEyeHull], -1, (0, 255, 0), 1)
+		cv2.drawContours(backtorgb, [rightEyeHull], -1, (0, 255, 0), 1)
 
 		# check to see if the eye aspect ratio is below the blink
 		# threshold, and if so, increment the blink frame counter
 		if ear < EYE_AR_THRESH:
 			COUNTER += 1
 
+			if COUNTER >= EYE_AR_CONSEC_FRAMES:
+				# TOTAL += 1
+				cv2.putText(frame, "ALERT!", (10, 30),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
 		# otherwise, the eye aspect ratio is not below the blink
 		# threshold
 		else:
 			# if the eyes were closed for a sufficient number of
-			# then increment the total number of blinks
-			if COUNTER >= EYE_AR_CONSEC_FRAMES:
-				TOTAL += 1
+			# then increm	ent the total number of blinks
 
 			# reset the eye frame counter
 			COUNTER = 0
 
 		# draw the total number of blinks on the frame along with
 		# the computed eye aspect ratio for the frame
-		cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-		cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+		# cv2.putText(frame, "Sleep count: {}".format(TOTAL), (10, 30),
+		# 	cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+		cv2.putText(frame, "EAR: {:.2f}".format(ear), (340, 240),
 			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
  
 	# show the frame
 	cv2.imshow("Frame", frame)
+	cv2.imshow("Grayscale",backtorgb)
+	cv2.imshow("Edge detection", auto_canny(gray))
 	key = cv2.waitKey(1) & 0xFF
  
 	# if the `q` key was pressed, break from the loop
